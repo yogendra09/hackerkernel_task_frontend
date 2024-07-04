b5357234f2ed2f66f21f72c79b8fe9a30496d39b
yogendra09 <yogendrayadav168@gmail.com>
2024-07-05 04:03:16 +0530

    search

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -9,7 +9,7 @@ const AddProduct = () => {
   const [price, setprice] = useState("");
   const addProductHandler = (e) => {
     e.preventDefault();
-    if (!product || !price && typeof(price) === 'number') return alert("enter product name and price");
+    if (!product || !price) return alert("enter product name and price");
     const newProduct = {
       id:nanoid(),
       product,
