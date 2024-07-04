956d0e1193d842c16b60734c661b4909e6cf2d14
yogendra09 <yogendrayadav168@gmail.com>
2024-07-05 00:47:12 +0530

    backend url updated

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api",
-  // withCredentials:true
+  baseURL: "https://hackerkernel-task-backend.onrender.com/api",
+  withCredentials:true
 });
 
 instance.interceptors.request.use(
